cd846bcd0ce65da29606a1b0394308274f2bbba7
Bai Yingjie <yingjie.bai@hxt-semitech.com>
2018-12-05 15:01:57 +0800

    pktgen: add a lua file to test multiflow
    usage:
       Pktgen:/> load /home/bai/repo/save/pktgen/flow.lua
       Pktgen:/> lua 'runFlowTest("single", 0, 0, 100, 64, 1, 1, 1, 10, 0)'
       Pktgen:/> lua 'runFlowTest("single", 0, 0, 100, 64, 1, 1, 1, 0, 100000000)'
       Pktgen:/> lua 'runFlowTest("multi", 0, 0, 100, 64, 2000, 128, 128, 10, 0)'

---
+++ pktgen/flow.lua
@@ -0,0 +1,107 @@
+package.path = package.path ..";?.lua;test/?.lua;app/?.lua;../?.lua"
+
+require "Pktgen";
+
+function number2ip(number)
+	local str=string.format("%08x", number);
+	local s4,s3,s2,s1 = str:match("(%x%x)(%x%x)(%x%x)(%x%x)");
+	return string.format("%d.%d.%d.%d", "0x"..s4,"0x"..s3,"0x"..s2,"0x"..s1);
+end
+
+function number2mac(number)
+	local str=string.format("%012x", number);
+	local s6,s5,s4,s3,s2,s1 = str:match("(%x%x)(%x%x)(%x%x)(%x%x)(%x%x)(%x%x)");
+	return string.format("%s:%s:%s:%s:%s:%s", s6,s5,s4,s3,s2,s1);
+end
+
+function setFlow(port, pkt_size, nb_L2, nb_L3, nb_L4)
+	pktgen.range.ip_proto(port, "udp");
+
+	pktgen.range.pkt_size(port, "start", pkt_size);
+	pktgen.range.pkt_size(port, "inc", 0);
+	pktgen.range.pkt_size(port, "min", pkt_size);
+	pktgen.range.pkt_size(port, "max", pkt_size);
+
+	pktgen.range.dst_mac(port, "inc", "00:00:00:00:00:00");
+
+	pktgen.range.src_mac(port, "start", "00:00:00:00:00:01");
+	pktgen.range.src_mac(port, "inc", "00:00:00:00:00:01");
+	pktgen.range.src_mac(port, "min", "00:00:00:00:00:01");
+	pktgen.range.src_mac(port, "max", number2mac(nb_L2));
+
+	pktgen.range.dst_ip(port, "start", "0.0.0.1");
+	pktgen.range.dst_ip(port, "inc", "0.0.0.1");
+	pktgen.range.dst_ip(port, "min", "0.0.0.1");
+	pktgen.range.dst_ip(port, "max", number2ip(nb_L3));
+
+	pktgen.range.src_ip(port, "inc", "0.0.0.0");
+
+	pktgen.range.dst_port(port, "inc", 0);
+
+	pktgen.range.src_port(port, "start", 1);
+	pktgen.range.src_port(port, "inc", 1);
+	pktgen.range.src_port(port, "min", 1);
+	pktgen.range.src_port(port, "max", nb_L4);
+
+	pktgen.set_range(port, "on");
+end
+
+-- run 64B signle flow 10s or 100000000 packets, full rate, log performance data to file "whatever_64B_1L2-1L3-1L4_flow.data"
+--   runFlowTest("whatever", 0, 0, 100, 64, 1, 1, 1, 10, 100000000)
+
+-- run from pktgen shell
+--   Pktgen:/> load /home/bai/repo/save/pktgen/flow.lua
+--   Pktgen:/> lua 'runFlowTest("single", 0, 0, 100, 64, 1, 1, 1, 10, 0)'
+--   Pktgen:/> lua 'runFlowTest("single", 0, 0, 100, 64, 1, 1, 1, 0, 100000000)'
+--   Pktgen:/> lua 'runFlowTest("multi", 0, 0, 100, 64, 2000, 128, 128, 10, 0)'
+
+function runFlowTest(mark, sendport, recvport, rate, pkt_size, nb_L2, nb_L3, nb_L4, duration, nb_pkts)  
+	sendport = tostring(sendport);
+	recvport = tostring(recvport);
+	local datafile = tostring(mark) .. "_" .. pkt_size .. "B_" .. nb_L2 .. "L2-" .. nb_L3 .. "L3-" .. nb_L4 .. "L4_flow.data"
+	local file = io.open(datafile, "w");
+
+	pktgen.stop("all");
+	pktgen.clr();
+	pktgen.set(sendport, "rate", rate);
+	pktgen.set(sendport, "count", nb_pkts);
+
+	setFlow(sendport, pkt_size, nb_L2, nb_L3, nb_L4);
+
+	pktgen.start(sendport);
+	print("Running flow test and collecting data to " .. datafile);
+
+	local rateTx, rateRx;
+	local ppsTxTable = {};
+	local ppsRxTable = {};
+	local i = 0;
+	repeat
+		pktgen.delay(1 * 1000);
+		rateTx = pktgen.portStats(sendport, "rate")[tonumber(sendport)];
+		rateRx = pktgen.portStats(recvport, "rate")[tonumber(recvport)];
+		i = i + 1;
+		ppsTxTable[i] = rateTx.pkts_tx;
+		ppsRxTable[i] = rateRx.pkts_rx;
+	until(i == duration or rateTx.pkts_tx == 0)
+
+	pktgen.stop(sendport);
+
+	pktgen.delay(1 * 1000);
+
+	local ppsTx = ppsTxTable[i//2];
+	local ppsRx = ppsRxTable[i//2];
+
+	local statTx = pktgen.portStats(sendport, "port")[tonumber(sendport)];
+	local statRx = pktgen.portStats(recvport, "port")[tonumber(recvport)];
+	local num_tx = statTx.opackets;
+	local num_rx = statRx.ipackets;
+	local num_dropped = num_tx - num_rx;
+
+	file:write("Tx pps: " .. ppsTx .. "\n");
+	file:write("Rx pps: " .. ppsRx .. "\n");
+	file:write("Tx pkts: " .. num_tx .. "\n");
+	file:write("Rx pkts: " .. num_rx .. "\n");
+	file:write("Dropped pkts: " .. num_dropped .. "\n");
+
+	file:close();
+end
